52eb322ece8a0bb34153d682eed994c5dbd634d3
Rebecca Turner <rbt@sent.as>
2024-08-30 17:02:07 -0700

    Add Garnix caching

--- flake.nix
+++ flake.nix
@@ -1,6 +1,13 @@
 {
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  nixConfig = {
+    extra-substituters = ["https://cache.garnix.io"];
+    extra-trusted-substituters = ["https://cache.garnix.io"];
+    extra-trusted-public-keys = ["cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="];
+  };
+
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system};
